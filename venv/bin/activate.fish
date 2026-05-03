41b8de5f2f3846c485146c4a60ffee036330af4d
luifrancisco <luigi.francisco@gmail.com>
2026-05-03 23:54:02 +0900

    reduced blog directory to root directory.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /workspace/8_stocks/blog/venv
+set -gx VIRTUAL_ENV /workspace/8_stocks/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
